7c8ec1868e061e462e799bb4cf5ee14ab54fa883
Suliman Fayyaz <hydrajin@gmail.com>
2022-01-18 01:37:58 -0500

    update LSB.js props

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -3,11 +3,12 @@ import React, { useState } from 'react';
 function LightSwitchButton(props) {
 //  const [light, setLight] = useState('off');
 // const handleClick = () => props.setLight(props.light === "on" ? "off" : "on");
+const {light, switchLight} = props;
 
 return (
-    <button onClick={props.switchLight} className="LightSwitchButton">
-      {props.light === "on" && <span><i>💡</i> I'm on!</span>}
-      {props.light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
+    <button onClick={switchLight} className="LightSwitchButton">
+      {light === "on" && <span><i>💡</i> I'm on!</span>}
+      {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
   </button>
   );
 }
